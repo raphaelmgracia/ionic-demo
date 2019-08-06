11c0f90bbee8bc7584cbea23df13b727f30ac66a
FazioNico <contact@nicolasfazio.ch>
2019-08-06 10:47:55 +0200

    upd: prepare users module features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'blog', loadChildren: () => import('./features/blog/blog.module').then( m => m.BlogModule)},
+  { path: 'users', loadChildren: () => import('./features/users/users.module').then( m => m.UsersModule)},
 ];
 
 @NgModule({
---
+++ src/app/features/users/containers/users-page/users-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-page',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.scss'],
+})
+export class UsersPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
