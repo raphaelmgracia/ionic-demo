854e21d4c150833e40057e6d782d16a401e38b70
FazioNico <contact@nicolasfazio.ch>
2019-08-07 10:31:06 +0200

    fix: change logic & add comments

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,21 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
 
   @Input() title: string;
-  @Input() backBtn: boolean = false;
-  constructor(private _location: Location) { }
 
-  ngOnInit() {}
-
-  back(){
-    console.log('TODO: back with router or Location (angular)...');
-    this._location.back();
-  }
 }
