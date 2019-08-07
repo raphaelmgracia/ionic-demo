027818af0174d4feac281ff209e1500fb348b72a
FazioNico <contact@nicolasfazio.ch>
2019-08-07 11:28:56 +0200

    upd: add click handler to custom header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,12 @@ export class HeaderComponent  {
   @Input() title: string;
   @Input() color?: string = 'primary';
 
+  @HostListener('click', ['$event.target']) onClick(target) {
+    const url = target.getAttribute('data-url');
+    if (!url) return;
+    console.log('Clicked on: ', target, url);
+    this._router.navigateByUrl(url);
+  }
+
+  constructor(private _router: Router) {}
 }
